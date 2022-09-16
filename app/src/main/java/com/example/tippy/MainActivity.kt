d5a42173c85a2e74e5150f4880f70582a48fa80b
HelenaHalldinITHS <helena.halldin@iths.se>
2022-09-16 10:13:48 +0200

    Make seekbar interactive

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.tippy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
+
+private const val TAG = "MainActivity";
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount: EditText
+    private lateinit var tvTipPercentLabel: TextView
+    private lateinit var sbTip: SeekBar
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
+        sbTip = findViewById(R.id.sbTip)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+
+        sbTip.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                Log.i(TAG, "onProgressChanged $p1")
+                tvTipPercentLabel.text = "$p1%"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+
+        })
     }
 }
