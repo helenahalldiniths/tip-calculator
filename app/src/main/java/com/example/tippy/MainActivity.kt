ec179dd087b5c80c68d4ec8f2343a26adffb8bcc
HelenaHalldinITHS <helena.halldin@iths.se>
2022-09-16 11:52:55 +0200

    Add tip description

--- app/src/main/java/com/example/tippy/MainActivity.kt
+++ app/src/main/java/com/example/tippy/MainActivity.kt
@@ -11,12 +11,14 @@ import android.widget.TextView
 
 private const val TAG = "MyMainActivity";
 private const val INITIAL_TIP_PERCENT = 10;
+
 class MainActivity : AppCompatActivity() {
     private lateinit var etBaseAmount: EditText
     private lateinit var tvTipPercentLabel: TextView
     private lateinit var sbTip: SeekBar
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,15 +28,17 @@ class MainActivity : AppCompatActivity() {
         sbTip = findViewById(R.id.sbTip)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
 
         sbTip.progress = INITIAL_TIP_PERCENT;
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
-
+        updateTipDescription(INITIAL_TIP_PERCENT)
         sbTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged $p1")
                 tvTipPercentLabel.text = "$p1%"
                 computeTipAndTotal()
+                updateTipDescription(p1)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -52,8 +56,19 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent){
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> { "Amazing"}
+        }
+        tvTipDescription.text = tipDescription
+    }
+
     private fun computeTipAndTotal() {
-        if(etBaseAmount.text.isEmpty()) {
+        if (etBaseAmount.text.isEmpty()) {
             tvTipAmount.text = ""
             tvTotalAmount.text = ""
             return
